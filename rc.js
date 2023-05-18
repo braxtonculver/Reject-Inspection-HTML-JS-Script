da2b44e9574893692bd951b048fe6ac440d73046
braxton <49820745+braxtonculver@users.noreply.github.com>
2023-05-18 14:40:08 -0400

    Add files via upload

---
+++ rc.js
@@ -0,0 +1,34 @@
+  $(document).on("contextmenu",function(e){
+    e.preventDefault();
+  });
+
+  $(document).keydown(function(event){
+    if(event.keyCode==123){
+      return false;
+    }
+      else if(event.ctrlKey && event.shiftKey && event.keyCode==73){
+        return false;
+    }
+  });
+
+  document.onkeydown = function(e) {
+    if (e.ctrlKey && (e.keyCode === 67 || e.keyCode === 86 || e.keyCode === 85 || e.keyCode === 117)) {
+  }
+    return false;
+  };
+
+  function click() {
+    if (event.button==2||event.button==3) {
+      oncontextmenu='return false';
+      alert("Property of Realm Studios // © 2018-2023");
+    }
+  }
+    document.onmousedown=click
+  document.oncontextmenu = new Function("return false;")
+
+  $("#js-flipInX").Morphext({
+    animation: "flipInX",
+      complete: function () {
+        console.log("This is called after a phrase is animated in! Current phrase index: " + this.index);
+      }
+  });
